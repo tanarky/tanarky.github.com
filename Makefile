92854eee4568e0407bcf9fe7b467cce6e0728897
Satoshi Tanaka <tanarky@gmail.com>
2011-11-03 13:24:10 +0900

    fixed design

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 .PHONY: install upload
 
 install:
-	rm -rf ./blog
-	cp -pr ./_blogofile/_site/* ./
+	./install.sh
 
 upload:
 	git checkout master
-	git commit -a -m "fixed something"
 	git push origin master
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SITEMAP=./sitemap.xml
+BLOGOFILEBIN=/home/satoshi/.virtualenvs/2.7.2/bin/blogofile
+
+cd ./_blogofile
+$BLOGOFILEBIN build
+cd ..
+rm -rf ./blog
+cp -pr ./_blogofile/_site/* ./
+echo "<urlset>" > $SITEMAP
+find ./ -name "*html" | perl -nle 'print "<url>http://tanarky.com$1</url>" if($_ !~ m|40[34]\.html| && $_ !~ m|^\./_| && m|^\.(.*)|)' >> $SITEMAP
+echo "</urlset>" >> $SITEMAP
