03c1cc48c0bb02b0a4611e1268083184ef233179
Satoshi Tanaka <tanarky@gmail.com>
2011-11-03 13:34:05 +0900

    fixed design

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ $BLOGOFILEBIN build
 cd ..
 rm -rf ./blog
 cp -pr ./_blogofile/_site/* ./
-echo "<urlset>" > $SITEMAP
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $SITEMAP
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $SITEMAP
 find ./ -name "*html" | perl -nle 'print "<url><loc>http://tanarky.com$1</loc></url>" if($_ !~ m|40[34]\.html| && $_ !~ m|^\./_| && m|^\.(.*)|)' >> $SITEMAP
 echo "</urlset>" >> $SITEMAP
