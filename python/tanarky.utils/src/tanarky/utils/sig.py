0a25f11886486a2a9800164ca2a3adb1af11c33d
Satoshi Tanaka <satoshi.tanaka@gree.net>
2013-01-11 19:10:23 +0900

    implement sig

--- python/tanarky.utils/src/tanarky/utils/sig.py
+++ python/tanarky.utils/src/tanarky/utils/sig.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 
-import base64
+import hashlib
 
-def gen():
-    return 'somesig'
+def gen(mes, seed='jjWeHNCpqnDkwlv6GRgbz2z1w5yunL6DKimEluQaW4k4j52gfubkb1PfoLyZE4Ug'):
+    return hashlib.sha1(mes+seed).hexdigest()[-16:]
+
+if __name__ == '__main__':
+    import logging
+    logging.getLogger().setLevel(logging.DEBUG)
+    print gen('foo')
+    print gen('bar')
--- python/tanarky.utils/test/test.py
+++ python/tanarky.utils/test/test.py
@@ -1,9 +1,46 @@
 # coding: utf-8
 
-import logging
-import tanarky.utils.sig
+import tanarky.utils.sig 
+import tanarky.utils.caesar
 
 if __name__ == '__main__':
+    import logging, random
     logging.getLogger().setLevel(logging.DEBUG)
-    logging.info(tanarky.utils.sig.gen())
 
+    # sig
+    sig = tanarky.utils.sig.gen('foo')
+    logging.info(sig)
+    logging.info(tanarky.utils.caesar.encode(sig))
+    logging.info(tanarky.utils.caesar.raw_encode(sig))
+
+    # caesar
+    enc = tanarky.utils.caesar.encode('foo')
+    logging.info(enc)
+    dec = tanarky.utils.caesar.decode(enc)
+    logging.info(dec)
+    n   = 120
+    enc = tanarky.utils.caesar.encode('foo', n)
+    logging.info(enc)
+    dec = tanarky.utils.caesar.decode(enc, n)
+    logging.info(dec)
+
+    # cookie.login
+    """
+    cookie_secret = 'zmVDRCilvXAcAe9R53IED3nLXlgaIuWw'
+    cookie_rot    = random.randrange(256)
+    cookie_login  = 'tanarky@yahoo.co.jp'
+    cookie_expsec = 60
+    c = tanarky.utils.cookie.Login()
+    c.get_name()
+    c.is_login()
+
+    secret=cookie_secret,
+    expire=cookie_expsec,
+    name=cookie_login,
+    rot=cookie_rot,
+    )
+
+    cookie_l = '...'
+    cookie_t = '...'
+    tanarky.utils.login.is_login(l=l, t=t)
+    """
